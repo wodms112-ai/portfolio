e935e430e8ed20d146114287f1fabf6a01c11062
wodms112-ai <wodms112@gmail.com>
2025-12-08 18:02:43 +0900

    .

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
     const myFullpage = new fullpage('#fullpage', {  /* html에서 페이지 전체를 감싸는 요소 */
 
-		navigation: true, /* 오른쪽에 각 페이지의 paging */
+		navigation: false, /* 오른쪽에 각 페이지의 paging */
 		navigationPosition: 'right', /* 위치 */
 		navigationTooltips: ['첫번째', '두번째', '세번째', '네번째'], /* 툴팁 */
 		showActiveTooltip: true, /* 현재 활성화된 페이지의 툴팁에 특정 클래스 주기 */
 		
-		lockAnchors: true,
+		lockAnchors: false,
 		anchors: ['link1', 'link2', 'link3', 'link4'], /* href="#link1" 이렇게 코딩하면 해당 링크명으로 이동 */
 
 		autoScrolling:true, /* 한페이지씩 스크롤 */
@@ -17,16 +17,21 @@ $(document).ready(function(){
 		scrollOverflow: false, /* 컨텐츠가 넘쳐도 스크롤 금지 */
 
 		afterLoad: function(origin, destination, direction, trigger){
-			if(destination.index == 2){ /* index가 2면 슬라이드는 세번째 슬라이드입니다. index 수는 0/1/2/3 */
-				console.log('3번째 슬라이드가 로딩 되었을때');
+			if(destination.index == 0){ /* index가 2면 슬라이드는 세번째 슬라이드입니다. index 수는 0/1/2/3 */
+				console.log('1번째 슬라이드가 로딩 되었을때');
+				$('header .gnb ul').hide()
+				
+			}else if(destination.index == 4){ /* index가 2면 슬라이드는 세번째 슬라이드입니다. index 수는 0/1/2/3 */
+				console.log('4번째 슬라이드가 로딩 되었을때');
+				$('header .gnb ul').hide()
+			}else {
+				console.log('기타등등')
+				$('header .gnb ul').show()
+				$('header').addClass('dark')
 			}
 		},
 
 		responsiveWidth: 640 /* fullpage를 적용시키지 않을 모바일 사이즈 */
 	});
 
-
-
-
-    
 })//맨끝
